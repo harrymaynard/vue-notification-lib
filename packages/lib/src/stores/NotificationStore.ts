ea82746ceea2a65e7e8adfb3106b533613bd6127
Harry Maynard <maynard321@gmail.com>
2025-02-15 21:36:56 -0800

    Formatting clean up.

--- packages/lib/src/stores/NotificationStore.ts
+++ packages/lib/src/stores/NotificationStore.ts
@@ -71,21 +71,21 @@ export const useNotificationStore = defineStore('notification-store', () => {
   const addNativeMessage = (
     message: ITextMessage
   ) => {
-    if (!("Notification" in window)) {
-      // Check if the browser supports notifications
-      alert("This browser does not support desktop notification")
-    } else if (Notification.permission === "granted") {
-      // Check whether notification permissions have already been granted;
-      // if so, create a notification
+    // Check if the browser supports notifications
+    if (!('Notification' in window)) {
+      console.error('This browser does not support desktop notification')
+    }
+    // Check whether notification permissions have already been granted;
+    // if so, create a notification
+    else if (Notification.permission === 'granted') {
       const notification = new Notification(message.text)
-      // …
-    } else if (Notification.permission !== "denied") {
+    }
+    else if (Notification.permission !== 'denied') {
       // We need to ask the user for permission
       Notification.requestPermission().then((permission) => {
         // If the user accepts, let's create a notification
-        if (permission === "granted") {
+        if (permission === 'granted') {
           const notification = new Notification(message.text)
-          // …
         }
       })
     }
